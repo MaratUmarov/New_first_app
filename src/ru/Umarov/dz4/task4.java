51ed9a947f8cfb6478d4c6020f1a4e87e34d2107
MaratUmarov <pirany10@gmail.com>
2020-03-31 02:09:20 +0300

    dz4 with task4 исправленно условие n

--- src/ru/Umarov/dz4/task4.java
+++ src/ru/Umarov/dz4/task4.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class task4 {
     public static void main(String[] args) {
 
-        System.out.println("Программа вывода арифметической прогрессии");
-        System.out.println("An=A1+d(n-1)\n" + "где n-кол-во членов пргрессии и d- разность прогрессии");
-        Scanner in = new
-                Scanner(System.in);
-        System.out.println("первый член вашей прогрессии a1= ");
-        int a1 = in.nextInt();
-        System.out.print("кол-во членов вашей прогресси n= ");
-        int n = in.nextInt();
-        System.out.print("разность прогрессии d= ");
-        int d = in.nextInt();
+        System.out.println("Программа вывода арифметической прогрессии" +
+                "An=A1+d(n-1)\n" + "где n-кол-во членов пргрессии и d- разность прогрессии\n" +
+                "надоест введите любую букву и все закончится\n");
+        while (true) {
+            Scanner in = new
+                    Scanner(System.in);
 
-        for (int i = 1; i <= n; i++) {
-            int mathProgress =  (a1 + d * (i - 1));
-            System.out.printf("%4d", mathProgress);
+            System.out.println("первый член вашей прогрессии a1= ");
+            boolean br = in.hasNextInt(); // прерывание по требованию
+            if (!br) break;
+            int a1 = in.nextInt();
+            System.out.print("кол-во членов вашей прогресси n= ");
+            int n = in.nextInt();
+            if (n <= 0) {
+                System.out.println("Не верные данные");
+                continue;
+            }
+            System.out.print("разность прогрессии d= ");
+            int d = in.nextInt();
+            for (int i = 1; i <= n; i++) {
+                int mathProgress = (a1 + d * (i - 1));
+                System.out.printf("%4d", mathProgress);
+            }
+            System.out.println();
+            System.out.println("Сумма данной прогрессии");
+            int mathSum = ((2 * a1 + d * (n - 1))) / 2 * n;
+            System.out.println(mathSum);
         }
-        System.out.println();
-        System.out.println("Сумма данной прогрессии");
-        int mathSum = ((2 * a1 + d * (n - 1))) / 2 * n;
-        System.out.println(mathSum);
-        in.close();
     }
 }
