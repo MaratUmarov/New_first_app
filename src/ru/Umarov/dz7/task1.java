39ab487a5035eda224d23a8a8681bd60f40f8c64
MaratUmarov <pirany10@gmail.com>
2020-04-08 23:03:01 +0300

    dz 7 fixed with draw

--- src/ru/Umarov/dz7/task1.java
+++ src/ru/Umarov/dz7/task1.java
@@ -28,85 +28,94 @@ public class task1 {
         }
         while (true) {
             System.out.println("Put Your money\n");
+            boolean f = scan.hasNextInt();
+            if (!f) {
+                System.out.println("You Should put money , no letters");
+               continue;
+                //break;
+            }
             int money = scan.nextInt();
+
             if (money < 30) {
                 System.out.println("Your money not enough, Please try again our break");
                 continue;
             }
-            String cha = "Don't forget Your change please";
-            System.out.println("Please choose Your Drink");
-            while (true) {
-                int choice = scan.nextInt();
-                int change;
-                switch (choice) {
-                    case 1:
-                        System.out.println(Drinks.COKE + " " + Drinks.COKE.s);
-                        if (money > 50) {
-                            change = money - 50;
-                            System.out.println(cha + "\n" + change);
-                        }
-                        if (money < 50) {
-                            System.out.println("Your money not enough, Please try again our break");
-                            continue;
-                        }
-                        break;
-                    case 2:
-                        System.out.println(Drinks.FANTA + " " + Drinks.FANTA.s);
-                        if (money > 50) {
-                            change = money - 50;
-                            System.out.println(cha + "\n" + change);
-                        }
-                        if (money < 50) {
-                            System.out.println("Your money not enough, Please try again our break");
-                            continue;
-                        }
-                        break;
-                    case 3:
-                        System.out.println(Drinks.ORANGE_JUICE + " " + Drinks.ORANGE_JUICE.s);
-                        if (money > 60) {
-                            change = money - 60;
-                            System.out.println(cha + "\n" + change);
-                        }
-                        if (money < 60) {
-                            System.out.println("Your money not enough, Please try again our break");
-                            continue;
-                        }
-                        break;
-                    case 4:
-                        System.out.println(Drinks.PEACH_JUICE + " " + Drinks.PEACH_JUICE.s);
-                        if (money > 70) {
-                            change = money - 70;
-                            System.out.println(cha + "\n" + change);
-                        }
-                        if (money < 70) {
-                            System.out.println("Your money not enough, Please try again our break");
-                            continue;
-                        }
-                        break;
-                    case 5:
-                        System.out.println(Drinks.GREEN_TEA + " " + Drinks.GREEN_TEA.s);
-                        if (money > 40) {
-                            change = money - 40;
-                            System.out.println(cha + "\n" + change);
-                        }
-                        if (money < 40) {
-                            System.out.println("Your money not enough, Please try again our break");
-                            continue;
-                        }
-                        break;
-                    case 6:
-                        System.out.println(Drinks.BLACK_TEA + " " + Drinks.BLACK_TEA.s);
-                        if (money > 30) {
-                            change = money - 30;
-                            System.out.println(cha + "\n" + change);
-                        }
-                    default:
-                        System.out.println("Your choice wrong try again");
+
+        String cha = "Don't forget Your change please";
+        System.out.println("Please choose Your Drink");
+        while (true) {
+            int choice = scan.nextInt();
+            int change;
+            switch (choice) {
+                case 1:
+                    System.out.println(Drinks.COKE + " " + Drinks.COKE.s);
+                    if (money > 50) {
+                        change = money - 50;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    if (money < 50) {
+                        System.out.println("Your money not enough, Please try again our break");
                         continue;
-                }
-                break;
+                    }
+                    break;
+                case 2:
+                    System.out.println(Drinks.FANTA + " " + Drinks.FANTA.s);
+                    if (money > 50) {
+                        change = money - 50;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    if (money < 50) {
+                        System.out.println("Your money not enough, Please try again our break");
+                        continue;
+                    }
+                    break;
+                case 3:
+                    System.out.println(Drinks.ORANGE_JUICE + " " + Drinks.ORANGE_JUICE.s);
+                    if (money > 60) {
+                        change = money - 60;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    if (money < 60) {
+                        System.out.println("Your money not enough, Please try again our break");
+                        continue;
+                    }
+                    break;
+                case 4:
+                    System.out.println(Drinks.PEACH_JUICE + " " + Drinks.PEACH_JUICE.s);
+                    if (money > 70) {
+                        change = money - 70;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    if (money < 70) {
+                        System.out.println("Your money not enough, Please try again our break");
+                        continue;
+                    }
+                    break;
+                case 5:
+                    System.out.println(Drinks.GREEN_TEA + " " + Drinks.GREEN_TEA.s);
+                    if (money > 40) {
+                        change = money - 40;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    if (money < 40) {
+                        System.out.println("Your money not enough, Please try again our break");
+                        continue;
+                    }
+                    break;
+                case 6:
+                    System.out.println(Drinks.BLACK_TEA + " " + Drinks.BLACK_TEA.s);
+                    if (money > 30) {
+                        change = money - 30;
+                        System.out.println(cha + "\n" + change);
+                    }
+                    break;
+                default:
+                    System.out.println("Your choice wrong try again");
+                    continue;
             }
             break;
         }
+        break;
     }
+}
 }
