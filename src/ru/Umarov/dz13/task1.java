49d1dccdc0cd9f01922061eaedfe996ba1f07ae3
MaratUmarov <pirany10@gmail.com>
2020-05-14 00:27:13 +0300

    dz13 fixed again

--- src/ru/Umarov/dz13/task1.java
+++ src/ru/Umarov/dz13/task1.java
@@ -1,10 +1,12 @@
 package ru.Umarov.dz13;
 
+import java.lang.Exception;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class EatExсeption extends Exception {
-    public EatExсeption() {
+class EatException extends Exception {
+    public void EatExсeption() {
+        String doNotLike = "Не буду это есть";
     }
 }
 
@@ -18,41 +20,32 @@ enum Food {
 }
 
 class Child {
-    public void Eat() {
-        System.out.println("Syel za obe cheki");
-    }
-
-    public void EatExсeptionAnswer() {
+    public void childAnswer() {
         String st = "Spasibo mamochka ochen vkusno";
         System.out.println(st);
+    }
+
+    public void Eat(String feedChild) throws EatException {
+        if (feedChild.equals("kasha") || feedChild.equals("shashlik") || feedChild.equals("yabloko")) {
+            System.out.println("Syel za obe cheki");
+        } else {
+            throw new EatException();
+        }
     }
 }
 
 public class task1 {
-    public static void main(String[] args) throws Exception {
-        Child e = new Child();
-        Child try_eat = new Child();
+    public static void main(String[] args) {
+        Child child = new Child();
         System.out.println(Arrays.toString(Food.values()));
         Scanner scan = new Scanner(System.in);
         System.out.println("Choose food for the Child from current List");
         String feedChild = scan.nextLine();
         try {
-            switch (feedChild) {
-                case "kasha":
-                case "shashlik":
-                case "yabloko":
-                    try_eat.Eat();
-                    break;
-                case "brokoli":
-                case "lemon":
-                case "kapusta":
-                    throw new EatExсeption();
-                default:
-                    throw new IllegalStateException("Unexpected value: " + feedChild);
-            }
+            child.Eat(feedChild);
         } catch (Exception ex) {
-            e.EatExсeptionAnswer();
         }
+        child.childAnswer();
     }
 }
 
