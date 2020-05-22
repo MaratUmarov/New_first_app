c91153aefcfa66841d444c681bb644713d689731
MaratUmarov <62353457+MaratUmarov@users.noreply.github.com>
2020-05-22 23:55:38 +0400

    fixed

--- src/ru/Umarov/dz16/task1.java
+++ src/ru/Umarov/dz16/task1.java
@@ -1,91 +1,17 @@
 package ru.Umarov.dz16;
 
 import java.io.*;
-import java.util.*;
-
-class Book implements Serializable {
-    private String name;
-    private String author;
-    private int year;
-
-    Book(String name, String author, int year) {
-        this.name = name;
-        this.author = author;
-        this.year = year;
-    }
-
-    String getName() {
-        return name;
-    }
-
-    String getAuthor() {
-        return author;
-    }
-
-    int getYear() {
-        return year;
-    }
-}
-
-class Library {
-    ArrayList<Book> addBook = new ArrayList<Book>();
-
-    public void addBook(Book b) {
-        this.addBook.add(b);
-    }
-
-    public void listBook() {
-        for (int i = 0; i < addBook.size(); i++)
-            System.out.println(addBook.get(i));
-    }
-
-    public void saveBook() throws IOException {
-        File dir = new File("Booklist");
-        String s = "listOfBooks.txt";
-        File bookfile = new File(dir, s);
-        boolean b = dir.mkdir();
-        try {
-            if (bookfile.createNewFile())
-                System.out.println("ok");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-        try {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(bookfile));
-            objectOutputStream.writeObject(addBook);
-            objectOutputStream.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void showList(File bookfile) throws FileNotFoundException {
-
-        ArrayList<Book> showBook = new ArrayList<>();
-        try (FileReader fileReader = new FileReader(bookfile)) {
-            System.out.println(fileReader);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        for (Book p : showBook)
-            System.out.println(p.getName() + "  " + p.getAuthor() + "  " + p.getYear());
-    }
-}
 
 public class task1 {
     public static void main(String[] args) throws Exception {
-
-
         Library library = new Library();
-
-        File bookfile=new File("Booklist","listOfBooks.txt");
         Book book1 = new Book("bore", "me", 2001);
         Book book2 = new Book("name", "author", 2002);
         System.out.println();
-        library.addBook(book1);
-        library.addBook(book2);
-        library.listBook();
+        library.Books(book1);
+        library.Books(book2);
         library.saveBook();
+        File bookfile = new File("Booklist", "listOfBooks.txt");
         library.showList(bookfile);
     }
 }
