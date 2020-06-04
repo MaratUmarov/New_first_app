c336eefbf9a3855c1c99a36c4f989bd2b2d4337c
MaratUmarov <pirany10@gmail.com>
2020-06-04 22:05:02 +0300

    dz21_2 fixed

--- src/ru/Umarov/dz21/task2.java
+++ src/ru/Umarov/dz21/task2.java
@@ -9,10 +9,13 @@ public class task2 {
         System.out.println();
     }
 
-    public static void swap(int[] a, int i1, int i2) {
-        int tmp = a[i1];
-        a[i1] = a[i2];
-        a[i2] = tmp;
+    static void reversArray(int[] arr) {
+        int temp;
+        for (int i = arr.length - 1, j = 0; i >= arr.length / 2; i--, j++) {
+            temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
@@ -24,13 +27,8 @@ public class task2 {
         System.out.println("введите элементы массива");
         for (int i = 0; i < n; i++) a[i] = sc.nextInt();
         print(a);
-        for (int left = 0; left < n; left++) {
-            int min = left;
-            for (int i = left; i < n; i++)
-                if (a[i] > a[min])
-                    min = i;
-            swap(a, left, min);
-            print(a);
-        }
+        reversArray(a);
+        print(a);
     }
 }
+
