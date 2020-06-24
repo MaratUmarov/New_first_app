f62f40e261b5c46c4668fd3f30588f44b4ca1f95
MaratUmarov <pirany10@gmail.com>
2020-06-25 00:56:08 +0300

    dz24 refactored

--- src/ru/Umarov/dz24/task1.java
+++ src/ru/Umarov/dz24/task1.java
@@ -1,15 +1,10 @@
 package ru.Umarov.dz24;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class task1 {
     public static void main(String[] args) {
-        String[] m = {"foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"};
-        Set<String> set = new LinkedHashSet<>();
-        Collections.addAll(set, m);
+        Set<String> set = new HashSet<>(Arrays.asList("foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"));
         for (String s : set) System.out.println(s);
         System.out.println("_________________");
         removeEvenLength(set);
@@ -17,11 +12,7 @@ public class task1 {
     }
 
     public static Set<String> removeEvenLength(Set<String> set) {
-        Iterator<String> iterator = set.iterator();
-        while (iterator.hasNext()) {
-            if (iterator.next().length() % 2 == 0)
-                iterator.remove();
-        }
+        set.removeIf(s -> s.length() % 2 == 0);
         return set;
     }
 }
